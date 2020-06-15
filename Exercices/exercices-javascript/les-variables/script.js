3e206acebbfea8c856c556c1542de19cbfc014fa
Joéllie Sprumont <joellie@siriusschool.be>
2020-06-15 16:01:20 +0200

    script-update

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,23 @@
+let helloWorld = 'Hello World';
+console.log(helloWorld);
+let hello = "Bonjour je m'appelle";
+const firstName = 'Joéllie';
+console.log(hello + ' ' + firstName);
+let have = "j'ai";
+let ageNumber = '29';
+let age = 'ans';
+console.log(hello + ' ' + firstName + ' ' + have + ' ' + ageNumber + ' ' + age);
+let siriusGroupArray = [
+  'Alexandre',
+  'Ezéchiel',
+  'Jason',
+  'Gilles',
+  'Jean Eudes',
+  'Jean Paul',
+  'Mohamed',
+  'Majda',
+  'Natascha',
+  'Joellie',
+];
+console.log(siriusGroupArray[6]);
+console.log(siriusGroupArray[9]);
