a87581c25e62861022d7aa20e8e40e49fa21ef15
Joéllie Sprumont <joellie@siriusschool.be>
2020-06-18 14:19:34 +0200

    Update-exercices-javaScript

--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -1,5 +1,29 @@
 //Insérez ici les exercices 2.1 à 2.4
 
+// Exercice 2.1
+
+function carre(a) {
+  return a * a;
+}
+console.log(carre(2));
+console.log(carre(4));
+console.log(carre(8));
+console.log(carre(16));
+
+//Exercice 2.2
+
+function divisionByTwo(c) {
+  let b = 2;
+  return c % b;
+}
+console.log(divisionByTwo(3));
+console.log(divisionByTwo(178));
+console.log(divisionByTwo(65));
+console.log(divisionByTwo(98));
+console.log(divisionByTwo(123));
+
+//Exercice 2.3
+
 // Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
 
 let firstNumber = 9;
@@ -9,18 +33,23 @@ let fourthNumber = 12;
 
 // Exercice incrémentation
 function incrementOnClick() {
-    document.getElementById("increment").addEventListener("click", function decrement() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let incrementCounter = 0;
+  document
+    .getElementById('increment')
+    .addEventListener('click', function decrement() {
+      // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+      let incrementCounter = 0;
 
-       console.log("Incrémentez") 
-})};
+      console.log('Incrémentez');
+    });
+}
 // Exercice incrémentation
 function decrementOnClick() {
-    document.getElementById("decrement").addEventListener("click", function increment() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let decrementCounter = 100;
+  document
+    .getElementById('decrement')
+    .addEventListener('click', function increment() {
+      // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+      let decrementCounter = 100;
 
-       console.log("Décrémentez");
-})};
-
+      console.log('Décrémentez');
+    });
+}
--- Exercices/exercices-javascript/les-variables/script.js
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -1,11 +1,14 @@
 let helloWorld = 'Hello World';
 console.log(helloWorld);
+
 let hello = "Bonjour je m'appelle";
 const firstName = 'Joéllie';
 console.log(hello + ' ' + firstName);
+
 let have = "j'ai";
 let ageNumber = '29 ans';
 console.log(hello + ' ' + firstName + ' ' + have + ' ' + ageNumber);
+
 let siriusGroupArray = [
   'Alexandre',
   'Ezéchiel',
@@ -20,8 +23,10 @@ let siriusGroupArray = [
   'Gilles',
   'Lionel',
 ];
+
 console.log(siriusGroupArray[6]);
 console.log(siriusGroupArray[9]);
+
 const student = { name: 'Sprumont', firstName: 'Joéllie', age: 29 };
 console.log(student);
 console.log(student.firstName);
