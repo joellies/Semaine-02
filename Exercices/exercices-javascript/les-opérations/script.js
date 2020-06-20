505a9435029b257f1541a2c4cfa580060faa0348
Joéllie Sprumont <joellie@siriusschool.be>
2020-06-20 21:26:46 +0200

    Update-javascript

--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -24,12 +24,53 @@ console.log(divisionByTwo(123));
 
 //Exercice 2.3
 
+function discountLamp(e) {
+  let price = 79.99;
+  let discount = 0.19;
+  return price - price * discount;
+}
+console.log(discountLamp(79.99));
+
 // Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
 
 let firstNumber = 9;
 let secondNumber = 8;
 let thirdNumber = 4;
 let fourthNumber = 12;
+
+//Exercice 2.4
+function divisionByFirstNumber() {
+  return (firstNumber /= f);
+}
+console.log(3);
+
+//Exercice 2.5
+
+function reassign() {
+  let h = firstNumber + secondNumber;
+  return (h += secondNumber);
+}
+console.log(reassign());
+
+// Exercice 2.6
+
+function reassignResult() {
+  let result = firstNumber * thirdNumber;
+  return (result += thirdNumber);
+}
+console.log(reassignResult());
+
+// Exercice 2.7
+// Soustrayez la variable firstNumber de votre fichier script.js à la variable fourthNumber et réassignez le résultat à fourthNumber, imprimez la dans votre console
+
+function reassignResult2() {
+  let result2 = firstNumber - fourthNumber;
+  return (result2 += fourthNumber);
+}
+console.log(reassignResult2());
+
+//Exercice 2.8
+// Au click sur ce bouton, incrémentez la variable incrementCounter et affichez le résultat dans votre console
 
 // Exercice incrémentation
 function incrementOnClick() {
@@ -38,18 +79,18 @@ function incrementOnClick() {
     .addEventListener('click', function decrement() {
       // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
       let incrementCounter = 0;
-
+      console.log(++incrementCounter);
       console.log('Incrémentez');
     });
 }
-// Exercice incrémentation
+// Exercice décrémentation
 function decrementOnClick() {
   document
     .getElementById('decrement')
     .addEventListener('click', function increment() {
       // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
       let decrementCounter = 100;
-
+      console.log(--decrementCounter);
       console.log('Décrémentez');
     });
 }
